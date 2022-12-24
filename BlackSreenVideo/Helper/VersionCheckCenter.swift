fae9b0799e10bde01956d2b0219ebcb677e53645
陳逸煌 <ehun9376@gmail.com>
2022-12-24 10:23:30 +0800

    相機畫面修正

--- BlackSreenVideo/Helper/VersionCheckCenter.swift
+++ BlackSreenVideo/Helper/VersionCheckCenter.swift
@@ -49,6 +49,33 @@ class VersionCheckCenter {
         }.resume()
     }
     
+    
 }
 
-
+extension String {
+    func versionCompare(_ otherVersion: String) -> ComparisonResult {
+        let versionDelimiter = "."
+        
+        var versionComponents = self.components(separatedBy: versionDelimiter)
+        
+        var otherVersionComponents = otherVersion.components(separatedBy: versionDelimiter)
+        
+        let diff = versionComponents.count - otherVersionComponents.count
+        
+        if diff == 0 {
+            
+            return self.compare(otherVersion, options: .numeric)
+        } else {
+            
+            let zeros = Array(repeating: "0", count: abs(diff))
+            if diff > 0 {
+                otherVersionComponents.append(contentsOf: zeros)
+            } else {
+                versionComponents.append(contentsOf: zeros)
+            }
+            
+            return versionComponents.joined(separator: versionDelimiter)
+                .compare(otherVersionComponents.joined(separator: versionDelimiter), options: .numeric)
+        }
+    }
+}
